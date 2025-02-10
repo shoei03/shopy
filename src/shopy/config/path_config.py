1aa76937d69d6b5617e73a1e33f025748e59a57d
shoei03 <shoei.yoshida.03@gmail.com>
2025-02-11 00:26:21 +0900

    パスの配置をわかりやすく変更

--- src/shopy/config/path_config.py
+++ src/shopy/config/path_config.py
@@ -5,18 +5,21 @@ from pathlib import Path
 @dataclass(frozen=True)
 class PathConfig:
     ORGANIZATION: str = "iluwatar"
-    REPO_NAME: str = "java-design-patterns"
-    ROOT_DIR: Path = Path(__file__).parents[3]
-    DATA_DIR: Path = ROOT_DIR / "data"
-    PROJECTS_DATA_DIR: Path = DATA_DIR / REPO_NAME
-    DELETED_FILES_DATA_DIR: Path = PROJECTS_DATA_DIR / "deleted_files"
-    DELETED_FILES: Path = DELETED_FILES_DATA_DIR / "files"
-    DELETED_FILES_INFO_CSV: Path = DELETED_FILES_DATA_DIR / "deleted_files_info.csv"
-    EXSISTING_FILES_DATA_DIR: Path = PROJECTS_DATA_DIR / "existing_files"
-    EXSISTING_FILES: Path = EXSISTING_FILES_DATA_DIR / "files"
-    EXSISTING_FILES_INFO_CSV: Path = EXSISTING_FILES_DATA_DIR / "existing_files_info.csv"
+    REPO_NAME:    str = "java-design-patterns"
+    
+    ROOT_DIR:     Path = Path(__file__).parents[3]
     PROJECTS_DIR: Path = ROOT_DIR.parent / "projects"
-    REPO_DIR: Path = PROJECTS_DIR / REPO_NAME
+    REPO_DIR:     Path = ROOT_DIR.parent / "projects" / REPO_NAME
+    
+    DATA_DIR:                 Path = ROOT_DIR / "data"
+    PROJECTS_DATA_DIR:        Path = ROOT_DIR / "data" / REPO_NAME
+    DELETED_FILES_DATA_DIR:   Path = ROOT_DIR / "data" / REPO_NAME / "deleted_files"
+    DELETED_FILES:            Path = ROOT_DIR / "data" / REPO_NAME / "deleted_files" / "files"
+    DELETED_FILES_INFO_CSV:   Path = ROOT_DIR / "data" / REPO_NAME / "deleted_files" / "deleted_files_info.csv"
+    EXISTING_FILES_DATA_DIR:  Path = ROOT_DIR / "data" / REPO_NAME / "existing_files"
+    EXISTING_FILES:           Path = ROOT_DIR / "data" / REPO_NAME / "existing_files" / "files"
+    EXISTING_FILES_INFO_CSV:  Path = ROOT_DIR / "data" / REPO_NAME / "existing_files" / "existing_files_info.csv"
+    
 
 
 path_config = PathConfig()
