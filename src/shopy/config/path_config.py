40666acc210e9e1e782114aad0b1c2245d57db62
shoei03 <shoei.yoshida.03@gmail.com>
2025-02-10 22:28:44 +0900

    パスを細分化

--- src/shopy/config/path_config.py
+++ src/shopy/config/path_config.py
@@ -9,6 +9,12 @@ class PathConfig:
     ROOT_DIR: Path = Path(__file__).parents[3]
     DATA_DIR: Path = ROOT_DIR / "data"
     PROJECTS_DATA_DIR: Path = DATA_DIR / REPO_NAME
+    DELETED_FILES_DATA_DIR: Path = PROJECTS_DATA_DIR / "deleted_files"
+    DELETED_FILES: Path = DELETED_FILES_DATA_DIR / "files"
+    DELETED_FILES_INFO_CSV: Path = DELETED_FILES_DATA_DIR / "deleted_files_info.csv"
+    EXSISTING_FILES_DATA_DIR: Path = PROJECTS_DATA_DIR / "existing_files"
+    EXSISTING_FILES: Path = EXSISTING_FILES_DATA_DIR / "files"
+    EXSISTING_FILES_INFO_CSV: Path = EXSISTING_FILES_DATA_DIR / "existing_files_info.csv"
     PROJECTS_DIR: Path = ROOT_DIR.parent / "projects"
     REPO_DIR: Path = PROJECTS_DIR / REPO_NAME
 
